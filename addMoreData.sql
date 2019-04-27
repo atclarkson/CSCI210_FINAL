3571720cb486e3244ecc5e46f70941764f521307
Adam <atclarkson@gmail.com>
2019-04-27 11:55:30 -0400

    Add records to sample database

---
+++ addMoreData.sql
@@ -0,0 +1,48 @@
+INSERT INTO angler (angler_id, angler_fname, angler_lname, angler_mname, angler_address1,
+  angler_address2, angler_city, angler_state, angler_zip, angler_phone1, angler_phone2)
+VALUES (4,'Mike','Rodgers','Sally','86 N Main St','Apt B6','Townland','IN','40404','3177721236','3338653636'),
+        (4,'Mike','Rodgers','Sally','86 N Main St','Apt B6','Townland','IN','40404','3177721236','3338653636'),
+        (4,'Mike','Rodgers','Sally','86 N Main St','Apt B6','Townland','IN','40404','3177721236','3338653636'),
+        (4,'Mike','Rodgers','Sally','86 N Main St','Apt B6','Townland','IN','40404','3177721236','3338653636'),
+        (4,'Mike','Rodgers','Sally','86 N Main St','Apt B6','Townland','IN','40404','3177721236','3338653636'),
+        (4,'Mike','Rodgers','Sally','86 N Main St','Apt B6','Townland','IN','40404','3177721236','3338653636'),
+        (4,'Mike','Rodgers','Sally','86 N Main St','Apt B6','Townland','IN','40404','3177721236','3338653636'),
+        (4,'Mike','Rodgers','Sally','86 N Main St','Apt B6','Townland','IN','40404','3177721236','3338653636'),
+        (4,'Mike','Rodgers','Sally','86 N Main St','Apt B6','Townland','IN','40404','3177721236','3338653636'),
+        (4,'Mike','Rodgers','Sally','86 N Main St','Apt B6','Townland','IN','40404','3177721236','3338653636'),
+        (4,'Mike','Rodgers','Sally','86 N Main St','Apt B6','Townland','IN','40404','3177721236','3338653636'),
+        (4,'Mike','Rodgers','Sally','86 N Main St','Apt B6','Townland','IN','40404','3177721236','3338653636'),
+        (4,'Mike','Rodgers','Sally','86 N Main St','Apt B6','Townland','IN','40404','3177721236','3338653636'),
+        (4,'Mike','Rodgers','Sally','86 N Main St','Apt B6','Townland','IN','40404','3177721236','3338653636'),
+        (4,'Mike','Rodgers','Sally','86 N Main St','Apt B6','Townland','IN','40404','3177721236','3338653636'),
+        (4,'Mike','Rodgers','Sally','86 N Main St','Apt B6','Townland','IN','40404','3177721236','3338653636'),
+        (4,'Mike','Rodgers','Sally','86 N Main St','Apt B6','Townland','IN','40404','3177721236','3338653636'),
+        (4,'Mike','Rodgers','Sally','86 N Main St','Apt B6','Townland','IN','40404','3177721236','3338653636'),
+        (4,'Mike','Rodgers','Sally','86 N Main St','Apt B6','Townland','IN','40404','3177721236','3338653636'),
+        (4,'Mike','Rodgers','Sally','86 N Main St','Apt B6','Townland','IN','40404','3177721236','3338653636'),
+        (4,'Mike','Rodgers','Sally','86 N Main St','Apt B6','Townland','IN','40404','3177721236','3338653636'),
+
+;INSERT INTO angler (angler_id,angler_fname,angler_lname,angler_mname,angler_address1,angler_city,angler_state,angler_zip,angler_phone1,angler_phone2) VALUES (5,"Zoe","Rich","Eliana","Ap #298-5227 Ultrices. Ave","Jacksonville","FL","42280","1-309-284-7304","1-898-309-2617"),(6,"Declan","Jennings","Wesley","162-9298 Adipiscing Rd.","Little Rock","AR","71157","1-923-484-6530","1-398-254-6724"),(7,"Kane","Ward","Alec","4309 Arcu. Ave","Metairie","LA","35861","1-764-417-3967","1-999-449-2173"),(8,"Josephine","Gordon","Holmes","P.O. Box 791, 5670 Bibendum Av.","Springfield","Massachusetts","42953","1-205-545-0300","1-835-573-5979"),(9,"Troy","Vargas","Hayley","Ap #792-8927 Dolor Av.","Glendale","Arizona","86150","1-471-768-0787","1-849-671-6551"),(10,"Brody","Zamora","Serena","Ap #693-2983 Augue St.","Springdale","Arkansas","72313","1-344-580-9859","1-365-318-9336"),(11,"Audrey","Ellison","Darius","283-1666 Proin St.","Kaneohe","Hawaii","77677","1-800-613-2185","1-106-204-6896"),(12,"Cooper","Sanchez","Hermione","P.O. Box 190, 3192 Justo Road","Saint Paul","Minnesota","44968","1-363-377-6593","1-363-217-6496"),(13,"Theodore","Tanner","Eve","811-3407 Quis Rd.","Metairie","LA","59750","1-655-883-5101","1-243-274-3638"),(14,"Emily","Carlson","Wyatt","Ap #608-9026 At Ave","Atlanta","Georgia","26731","1-229-581-5554","1-801-420-9939"),(15,"Grace","Beach","Ethan","Ap #569-309 Orci Avenue","Fort Collins","CO","72483","1-169-555-2948","1-110-824-2834"),(16,"Aiko","Boone","Farrah","8653 Sed Av.","Cedar Rapids","Iowa","67325","1-220-535-4695","1-500-725-7411"),(17,"Quon","Mullen","Cooper","9711 Dictum Rd.","Tucson","Arizona","86754","1-884-641-6150","1-611-849-3811"),(18,"Savannah","West","Kelly","Ap #196-3555 Neque. Ave","West Jordan","UT","85612","1-259-977-7305","1-830-665-5045"),(19,"Kaden","Maldonado","Rhea","356-6542 Cursus Avenue","Topeka","KS","78055","1-453-932-3391","1-889-174-3231"),(20,"Kelsey","Atkinson","Edan","4969 Quis, St.","Gillette","WY","83702","1-986-270-2179","1-264-340-8414"),(21,"Hedley","Castro","Kevin","Ap #695-227 Felis Avenue","Mobile","AL","35352","1-328-978-5604","1-510-853-7935"),(22,"Kareem","Cantu","Britanney","Ap #505-2566 Natoque Street","Wilmington","DE","27648","1-317-789-7340","1-755-426-6650"),(23,"Branden","Travis","Susan","P.O. Box 469, 9673 Cras Road","Pocatello","Idaho","43158","1-254-273-0036","1-165-298-8741"),(24,"Guy","Nieves","Andrew","P.O. Box 514, 9266 Diam. St.","South Portland","ME","81564","1-458-919-6761","1-825-199-0132"),(25,"Hall","Chang","Jordan","664-8740 Lorem, Street","Idaho Falls","Idaho","19501","1-272-427-4487","1-564-536-9615"),(26,"Wylie","Huff","Ramona","Ap #547-6140 Amet, St.","San Jose","California","90300","1-274-310-0929","1-199-363-3439"),(27,"Channing","Cardenas","Belle","250-6849 Luctus. Avenue","Norman","OK","26806","1-140-449-5552","1-670-762-5304"),(28,"Philip","Workman","Emily","431-4751 Ac Av.","Detroit","MI","44915","1-106-783-7183","1-611-558-3891"),(29,"Lareina","Ortega","Geraldine","540-6617 Est, Ave","Cincinnati","OH","33162","1-485-513-2985","1-815-912-3264");
+
+
+INSERT INTO registration (angler_id, tourn_id)
+VALUES (1,1),(1,2),(1,3),(2,6),(5,6),(4,4),(4,5),(5,1),(6,1),(7,1),(8,1),(9,1),(10,1),(11,1),(12,1),(13,1),(14,1),(15,1),(16,1),(17,1),(18,1),(19,1),(20,1);
+
+INSERT INTO weighin (angler_id, tourn_id, weigh_timestamp, weigh_weight, weigh_numfish, weigh_shortfish, weigh_deadfish, weigh_minlate)
+VALUES (1,1, '2019-08-01 15:00:00', 15.63, 4,0,0,1),
+(5,1, '2019-08-01 15:00:00', 5.45, 4,0,0,1),
+(6,1, '2019-08-01 15:00:00', 22.63, 4,0,0,1),
+(7,1, '2019-08-01 15:00:00', 16.78, 4,0,0,1),
+(8,1, '2019-08-01 15:00:00', 5.15, 4,0,0,1),
+(9,1, '2019-08-01 15:00:00', 9.45, 4,0,0,1),
+(10,1, '2019-08-01 15:00:00', 11.36, 4,0,0,1),
+(11,1, '2019-08-01 15:00:00', 13.75, 4,0,0,1),
+(12,1, '2019-08-01 15:00:00', 25.91, 4,0,0,1),
+(13,1, '2019-08-01 15:00:00', 6.46, 4,0,0,1),
+(14,1, '2019-08-01 15:00:00', 1.94, 4,0,0,1),
+(15,1, '2019-08-01 15:00:00', 18.76, 4,0,0,1),
+(16,1, '2019-08-01 15:00:00', 18.28, 4,0,0,1),
+(17,1, '2019-08-01 15:00:00', 13.55, 4,0,0,1),
+(18,1, '2019-08-01 15:00:00', 12.66, 4,0,0,1),
+(19,1, '2019-08-01 15:00:00', 9.34, 4,0,0,1),
+(20,1, '2019-08-01 15:00:00', 7.75, 4,0,0,1);
